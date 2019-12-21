8bf094b9fae570bd1c34aaca7facf48426e4c233
Sandupa Dalugoda <sandupasanthusha@gmail.com>
2019-12-21 16:42:13 +0530

    final changes

--- src/screen/game.js
+++ src/screen/game.js
@@ -10,18 +10,23 @@ class Game extends React.Component {
 			sliderValue: 0,
 			grid: Array(9).fill(null),
 			score: 0,
-			counterIncrement: 1
+			counterIncrement: 1,
+			won: true
 		};
 	}
 
 	componentDidMount() {
 		//Set Grid to 0
-		let grid = this.state.grid;
-		grid[0] = true;
-		this.setState({ grid: grid });
+		this.resetGrid();
 
 		//Start Slider
 		this.startAnimation();
+	}
+
+	resetGrid() {
+		let grid = Array(9).fill(null);
+		grid[0] = true;
+		this.setState({ grid: grid });
 	}
 
 	startAnimation() {
@@ -50,48 +55,54 @@ class Game extends React.Component {
 		let { score, sliderValue, grid } = this.state;
 
 		//Marks will be given if slider is between 40 & 60
-		if (sliderValue > 40 && sliderValue < 60) {
+		if (sliderValue > 20 && sliderValue < 80) {
 			score += 10;
 			const currentPosition = grid.findIndex(value => value === true);
 
-			if (currentPosition === 8) {
-				//game over
-			} else {
-				//Move the player to next box
-				grid = Array(9).fill(null);
-				grid[currentPosition + 1] = true;
-			}
+			//Move the player to next box
+			grid = Array(9).fill(null);
+			grid[currentPosition + 1] = true;
+
 			this.setState({ score: score, grid: grid });
+
+			if (currentPosition === 7) {
+				alert("You Won !!!");
+
+				setTimeout(() => {
+					this.resetGrid();
+					this.setState({ score: 0 });
+				}, 3000);
+			}
 		} else {
 			//Else reset the player to first box
-			grid = Array(9).fill(null);
-			grid[0] = true;
-			this.setState({ grid: grid });
+			this.resetGrid();
 		}
 	}
 
 	render() {
 		return (
 			<div className="container">
-				<input
-					type="range"
-					onChange={this.sliderOnChange}
-					min="1"
-					max="100"
-					value={this.state.sliderValue}
-					className="slider"
-				/>
+				<div className="slider-container">
+					<div className="slider-divider"></div>
+					<input
+						type="range"
+						min="1"
+						max="100"
+						value={this.state.sliderValue}
+						className="slider"
+						readOnly
+					/>
+				</div>
 				<div className="grid-container">
 					{this.state.grid.map((item, index) => {
 						return (
 							<div className="grid-item" key={index}>
 								<div className="grid-item-inner">
-									<div>{index}</div>
+									<div className="box-number">{index}</div>
 									{item === true && (
-										<div>
+										<div className="im-here">
 											<img
 												src={require("../assets/marker.png")}
-												className="im-here"
 												alt="I'm here"
 											/>
 										</div>
